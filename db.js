11352c7de2b9e5ae53b751e861af767899eb2b08
gourav <gouravsharma730@gmail.com>
2024-04-02 22:13:18 +0530

    dot

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/carWash',{useNewUrlParser:true,useUnifiedTopology:true});
 
 const db =  mongoose.connection;
-db.on('error',()=>console.log('MongoDB connection error'));
+db.on('error',()=>console.log('MongoDB connection error.'));
 db.on('open',()=>console.log('mongoDB connected.'))
 
 
