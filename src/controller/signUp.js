93bf2c98c7013ddd130570867d5480325e2f2789
gourav <gouravsharma730@gmail.com>
2024-04-25 13:09:20 +0530

    token saved in the broswer and admin dashboard modified

--- src/controller/signUp.js
+++ src/controller/signUp.js
@@ -20,11 +20,11 @@ const signUp = async function(req,res){
         let user = new User({userName,email,password:hashedPassword,address,phoneNumber});
         await user.save();
         const token = jwt.sign({ _id: user._id }, 'your_secret_key', { expiresIn: '700h' });
-        const message =[{message:`Hi ${userName}, Signup successful!!`},{ token: token }]
+        const message =[{message:`Hi ${userName}, Signup successful!!`}]
         const subject='Welcome to SpeedyShine - Confirm Your Registration';
         const text =`Hello ${userName}, this is a confirmation email.`;
         const mailCheck = await sendEmail(email,subject,text,htmlFilePath,userName);
-        return res.status(201).json({message:message});
+        return res.status(201).json({message,token});
     }catch(error){
         return res.status(500).json({message: error.message})
     }
